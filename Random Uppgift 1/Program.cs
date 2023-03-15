e392d3ffc96c3d8944a357f7bcd3e1fc0081a59e
Anton <>
2023-03-15 10:18:21 +0100

    Add project files.

---
+++ Random Uppgift 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Välj ett tal mellan 1 och 10 : ");
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            Random slump = new Random();
+
+            int snurr = slump.Next(1,11);
+
+            Console.WriteLine("Lyckohjulet fick " + snurr);
+
+            if (val==snurr)
+            {
+                Console.WriteLine("Du vann! ");
+            } 
+            else
+            {
+                Console.WriteLine("Du förlorade! ");
+            }
+
+
+
+
+        }
+    }
+}
